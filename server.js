f44bada9d74d4f47e87505abb4c7a4b3adede494
Joann <joannpelza0@gmail.com>
2020-03-14 01:04:15 +0530

    authentication

--- server.js
+++ server.js
@@ -1,56 +1,31 @@
-const express=require('express')
-const app=express()
-const port=8080;
-const bcrypt=require('bcrypt')
-//const session=require('express-session');
+var express=require("express")
+var app=express()
 
-var cors=require('cors');
-var bodyParser=require('body-parser');
-
+var cors=require("cors")
+var bodyParser=require("body-parser")
+var mongoose=require("mongoose")
+var port=process.env.PORT || 8080
 
 
-var MongoClient=require('mongodb').MongoClient;             //Mongodb Connection
-var url="mongodb://localhost:27017/";
 
-MongoClient.connect(url, function(err, db){
-    if(err) throw err;
-    var dbo=db.db("admindb");
-    dbo.createCollection("user",function(err,res){
-        if(err) throw err;
-        console.log("Collection created");
-        db.close();
-    });
-});
-
-
-app.use(cors());
-app.options('*',cors());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());                 //json converted to object
-
-app.get('/',function(req,res){
-    connection.query('select 1+1 AS Solution',function(err,result,fields){
-        if (err) throw err;
-        console.log("Solution is " + result[0].Solution);
+app.use(bodyParser.json())
+app.use(cors())
+app.use(
+    bodyParser.urlencoded({
+        extended: false
     })
-    res.send('hello');
-})
-app.post('/signup',function(req,res){
-    var responseData=req.body;
-    var username=responseData.email;
-    var password=responseData.password;
+    )
 
-    var data={
-        "email":email,
-        "password":password
-    }
+const url="mongodb://localhost:27017/admindb"
+mongoose
+.connect(url,{useNewUrlParser:true})
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err))
 
-    db.collection('userdetails').insertOne(data,function(err,collection){
-        if (err) throw err;
-        console.log("Record inserted successfully");
-    });
+var Users=require('./routes/Users')
+
+app.use('/',Users)
+
+app.listen(port, () => {
+    console.log("Server is running on port : " + port)
 })
-    app.get('/',function(req,res){
-        res.set({'Access-control-Allow-Origin': '*'});
-    }).listen(8000)
-    console.log("server listening on port 8000");
