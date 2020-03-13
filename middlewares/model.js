311ae67e7a704067499e346a97b04cd92089061b
Shriram Venugopal <shriramvenugopal123@gmail.com>
2020-03-13 13:46:20 +0530

    started working on middleware integration

---
+++ middlewares/model.js
@@ -0,0 +1,42 @@
+// this is the middleware function which checks for any kind of intrusions.
+
+
+module.exports = async (req, res, next) => {
+
+    // get the packet details.
+     
+
+
+
+    // preprocess the data
+
+
+
+
+    // run the model with the test data
+
+
+
+
+    // obtain the result
+
+
+
+
+    // parse the result
+
+
+
+
+    // post processing
+
+
+
+
+    // forward or discard packet.
+
+    next()
+
+
+
+}
