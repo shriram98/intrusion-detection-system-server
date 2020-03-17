eba5ef24209cc14b3b60f2831beb2bec92391f47
Shriram Venugopal <shriramvenugopal123@gmail.com>
2020-03-17 22:11:42 +0530

    added python scripts

--- middlewares/model.js
+++ middlewares/model.js
@@ -1,15 +1,32 @@
 // this is the middleware function which checks for any kind of intrusions.
 
+const {spawn} = require('child_process')
 
 module.exports = async (req, res, next) => {
 
+    
     // get the packet details.
-     
+    
+    var packetJson = req.body.jsonFile
+    var packetCSV = req.body.csvFile
 
 
 
     // preprocess the data
 
+    // result of the prediction
+    var result
+
+    //spawn new child process to call the python script
+    // const python = spawn('python3', ["../scripts/model/script.py"])
+    // python.stdout.on('data', async (data) => {
+    //     console.log('started')
+    //     console.log(data.toString());
+    // })
+
+    // python.on('close', (code) => {
+    //     console.log('closed')
+    // })
 
 
 
