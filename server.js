34458b9f900a56ed0a3e551e3d9a44e26f5e0eab
Joann <joannpelza0@gmail.com>
2020-03-19 04:56:57 +0530

    added functions

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 var express=require("express")
 var app=express()
-
 var cors=require("cors")
 var bodyParser=require("body-parser")
 var mongoose=require("mongoose")
-var port=process.env.PORT || 8080
-
+var port=process.env.PORT ||5000
 
 
 app.use(bodyParser.json())
@@ -16,16 +14,15 @@ app.use(
     })
     )
 
-const url="mongodb://localhost:27017/admindb"
-mongoose
-.connect(url,{useNewUrlParser:true})
+var url="mongodb://localhost:27017/admindb"
+mongoose.connect(url,{useNewUrlParser:true})
 .then(() => console.log("MongoDB connected"))
 .catch(err => console.log(err))
 
 var Users=require('./routes/Users')
-
 app.use('/',Users)
 
 app.listen(port, () => {
     console.log("Server is running on port : " + port)
 })
+
